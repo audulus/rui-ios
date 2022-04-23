36b763a2101c69357680bbea3130b759afb246e0
Taylor Holliday <taylor@audulus.com>
2022-04-23 10:20:04 -0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+use rui::*;
+
+// Ultimately we'd like to use swift-bridge, once it's ready.
+
+use std::alloc::{self, Layout};
+
+#[no_mangle]
+pub extern fn new_context() -> *mut Context {
+    let ptr = unsafe { alloc::alloc(Layout::new::<Context>()) };
+    ptr as *mut Context
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
