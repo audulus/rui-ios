abbbe8973b5779c3e55a86eb3854f2a3513dd8d2
Taylor Holliday <taylor@audulus.com>
2022-04-23 10:37:13 -0700

    satisfy cbindgen

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,22 @@ use rui::*;
 
 // Ultimately we'd like to use swift-bridge, once it's ready.
 
-#[no_mangle]
-pub extern fn new_context() -> *mut Context {
-    Box::into_raw(Box::new(Context::new()))
+pub struct AppState {
+    cx: Context
+}
+
+impl AppState {
+    pub fn new() -> Self {
+        Self{ cx: Context::new() }
+    }
 }
 
 #[no_mangle]
-pub extern fn delete_context(cx: *mut Context) {
+pub extern fn new_context() -> *mut AppState {
+    Box::into_raw(Box::new(AppState::new()))
+}
+
+#[no_mangle]
+pub extern fn delete_context(cx: *mut AppState) {
     unsafe { Box::from_raw(cx); }
 }
