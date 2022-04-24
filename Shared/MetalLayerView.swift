fa5f9d57d79fa4787533197a47bdb0ee6419366d
Taylor Holliday <taylor@audulus.com>
2022-04-23 20:52:51 -0700

    Update MetalLayerView.swift

--- Shared/MetalLayerView.swift
+++ Shared/MetalLayerView.swift
@@ -7,10 +7,25 @@ struct MetalLayerView: UIViewRepresentable {
 
     var appState: OpaquePointer
 
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+
+    class Coordinator: NSObject {
+        var layer: CAMetalLayer?
+    }
+
     func makeUIView(context: Context) -> UIView {
         let view = UIView()
+        view.frame.size = CGSize(width: 1024, height: 768)
 
         var metalLayer = CAMetalLayer()
+        metalLayer.device = MTLCreateSystemDefaultDevice()!
+        metalLayer.pixelFormat = .bgra8Unorm_srgb
+        metalLayer.frame = view.layer.frame
+        metalLayer.framebufferOnly = true
+
+        context.coordinator.layer = metalLayer
 
         setup_surface(appState, &metalLayer)
 
@@ -24,6 +39,18 @@ struct MetalLayerView: UIViewRepresentable {
 
         let size = uiView.frame.size
         let scale = uiView.contentScaleFactor
+
+        print("uiView size: \(size)")
+        print("scale: \(scale)")
+
+        if let metalLayer = context.coordinator.layer {
+            metalLayer.frame = uiView.layer.frame
+
+            metalLayer.drawableSize = CGSize(width: size.width * scale, height: size.height * scale)
+
+            print("drawableSize: \(metalLayer.drawableSize)")
+        }
+
         render(appState, Float(size.width), Float(size.height), Float(scale))
     }
 
