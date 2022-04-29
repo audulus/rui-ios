dd639d660704a95e8283b767bee84b9a7351f65a
Taylor Holliday <taylor@audulus.com>
2022-04-28 18:13:55 -0700

    Use MTKView

--- Shared/MetalLayerView.swift
+++ Shared/MetalLayerView.swift
@@ -2,6 +2,29 @@
 import SwiftUI
 import Metal
 import QuartzCore
+import MetalKit
+
+class Renderer: NSObject, MTKViewDelegate {
+
+    var appState: OpaquePointer
+
+    init(appState: OpaquePointer) {
+        self.appState = appState
+    }
+
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+        // do nothing
+    }
+
+    func draw(in view: MTKView) {
+        let size = view.frame.size
+        let scale = view.contentScaleFactor
+        update(appState, Float(size.width), Float(size.height))
+        render(appState, Float(size.width), Float(size.height), Float(scale))
+    }
+
+
+}
 
 struct MetalLayerView: UIViewRepresentable {
 
@@ -12,49 +35,24 @@ struct MetalLayerView: UIViewRepresentable {
     }
 
     class Coordinator: NSObject {
-        var layer: CAMetalLayer?
+        var renderer: Renderer?
     }
 
     func makeUIView(context: Context) -> UIView {
-        let view = UIView()
-        view.frame.size = CGSize(width: 1024, height: 768)
-
-        var metalLayer = CAMetalLayer()
-        metalLayer.device = MTLCreateSystemDefaultDevice()!
-        metalLayer.pixelFormat = .bgra8Unorm_srgb
-        metalLayer.frame = view.layer.frame
-        metalLayer.framebufferOnly = true
-
-        context.coordinator.layer = metalLayer
-
+        let view = MTKView(frame: CGRect(x: 0, y: 0, width: 1024, height: 768), device: MTLCreateSystemDefaultDevice())
+        var metalLayer = view.layer as! CAMetalLayer
         setup_surface(appState, &metalLayer)
 
-        view.layer.addSublayer(metalLayer)
+        let renderer = Renderer(appState: appState)
+        context.coordinator.renderer = renderer
+        view.delegate = renderer
+
         return view
     }
 
     func updateUIView(_ uiView: UIView, context: Context) {
         // do nothing
-        print("updateUIView")
 
-        let size = uiView.frame.size
-        let scale = UIScreen.main.scale
-        uiView.contentScaleFactor = scale
-
-        print("uiView size: \(size)")
-        print("scale: \(scale)")
-
-        if let metalLayer = context.coordinator.layer {
-            metalLayer.frame = uiView.layer.frame
-
-            metalLayer.drawableSize = CGSize(width: size.width * scale, height: size.height * scale)
-
-            print("drawableSize: \(metalLayer.drawableSize)")
-        }
-
-        update(appState, Float(size.width), Float(size.height))
-
-        render(appState, Float(size.width), Float(size.height), Float(scale))
     }
 
 }
