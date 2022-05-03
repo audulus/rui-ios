beebd34e69765c320819c268a4364be134786dd2
Taylor Holliday <taylor@audulus.com>
2022-05-03 13:53:11 -0700

    Add Bridging-Header.h

---
+++ Bridging-Header.h
@@ -0,0 +1,8 @@
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#include "Generated/SwiftBridgeCore.h"
+#include "Generated/ios-rust-analyzer/ios-rust-analyzer.h"
+
+#endif /* Bridging_Header_h */
