9cc077bac48f67aa20cc2d08d8e6277422536d08
Taylor Holliday <taylor@audulus.com>
2022-05-04 07:22:20 -0700

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
+use core::ffi::c_void;
 use futures::executor::block_on;
 use rui::*;
 use vger::*;
-use core::ffi::c_void;
 
 fn my_ui() -> impl View {
     vstack(("Hello", "world"))
@@ -16,15 +16,19 @@ pub struct AppState {
 
 impl AppState {
     pub fn new() -> Self {
-        Self { cx: Context::new(), setup: None, config: None, vger: None }
+        Self {
+            cx: Context::new(),
+            setup: None,
+            config: None,
+            vger: None,
+        }
     }
 
     pub fn setup_surface(&mut self, ca_layer_ptr: *mut c_void) {
-    
         println!("ca_layer_ptr: {:?}", ca_layer_ptr);
-    
-        let setup = block_on(setup(unsafe { * (ca_layer_ptr as *mut *mut c_void) }));
-    
+
+        let setup = block_on(setup(unsafe { *(ca_layer_ptr as *mut *mut c_void) }));
+
         let config = wgpu::SurfaceConfiguration {
             usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
             format: setup.surface.get_preferred_format(&setup.adapter).unwrap(),
@@ -33,27 +37,28 @@ impl AppState {
             present_mode: wgpu::PresentMode::Mailbox,
         };
         setup.surface.configure(&setup.device, &config);
-    
-        self.vger = Some(Vger::new(&setup.device, wgpu::TextureFormat::Bgra8UnormSrgb));
-    
+
+        self.vger = Some(Vger::new(
+            &setup.device,
+            wgpu::TextureFormat::Bgra8UnormSrgb,
+        ));
+
         self.setup = Some(setup);
         self.config = Some(config);
-        
     }
 
     pub fn update(&mut self, width: f32, height: f32) {
         let mut access_nodes = vec![];
-    
+
         self.cx.update(
             &my_ui(),
             &mut self.vger.as_mut().unwrap(),
             &mut access_nodes,
-            [width, height].into()
+            [width, height].into(),
         );
     }
 
     pub fn render(&mut self, width: f32, height: f32, scale: f32) {
-    
         if let Some(setup) = &self.setup {
             self.cx.render(
                 &setup.device,
@@ -66,22 +71,21 @@ impl AppState {
                 scale,
             );
         }
-        
     }
 }
 
 #[swift_bridge::bridge]
 mod ffi {
-   extern "Rust" {
-      type AppState;
+    extern "Rust" {
+        type AppState;
 
-      #[swift_bridge(init)]
-      fn new() -> AppState;
+        #[swift_bridge(init)]
+        fn new() -> AppState;
 
-      fn setup_surface(&mut self, ca_layer_ptr: *mut c_void);
-      fn update(&mut self, width: f32, height: f32);
-      fn render(&mut self, width: f32, height: f32, scale: f32);
-   }
+        fn setup_surface(&mut self, ca_layer_ptr: *mut c_void);
+        fn update(&mut self, width: f32, height: f32);
+        fn render(&mut self, width: f32, height: f32, scale: f32);
+    }
 }
 
 struct Setup {
@@ -124,4 +128,3 @@ async fn setup(ca_layer_ptr: *mut core::ffi::c_void) -> Setup {
         queue,
     }
 }
-
